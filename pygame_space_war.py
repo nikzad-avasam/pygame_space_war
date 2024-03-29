2fc97ab6d8a05653dc82dda5ec40b2d79a77c23b
sam nikzad <abbas.nikzad70@gmail.com>
2024-03-29 20:21:59 +0330

    Update pygame_space_war.py

--- pygame_space_war.py
+++ pygame_space_war.py
@@ -1,1 +1,12 @@
+import pygame
 
+pygame.init()
+
+# main screen 
+screen = pygame.display.set_mode((800,400))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
